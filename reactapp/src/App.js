53f0801b6601d68db2984262d500d1fdce98575f
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-09-22 16:39:28 +0200

    updated

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react'
+import AddUser from './components/AddUser'
+import Users from './components/Users'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      users:[
+        {
+          id:1,
+          name: "Hasan",
+          email: "hasan@gmail.com"
+        },
+        {
+          id:2,
+          name: "Hakan",
+          email: "hakan@gmail.com"
+        },
+        {
+          id:3,
+          name: "Fahri",
+          email: "fahri@gmail.com"
+        },
+        {
+          id:4,
+          name: "Bünyamin",
+          email: "bünyamin@gmail.com"
+        },
+        {
+          id:5,
+          name: "Ibrahim",
+          email: "ibrahim@gmail.com"
+        }
+      ]
+    }
+  }
   render() {
     return (
-      <div>
-        <h5>Default Text</h5>
-        <p>This is sample Text</p>
+      <div className="container">
+        <h4>User App</h4>
+        <hr />
+        <AddUser/>
+        <hr />
+        <Users users= {this.state.users}/>
       </div>
     )
   }
